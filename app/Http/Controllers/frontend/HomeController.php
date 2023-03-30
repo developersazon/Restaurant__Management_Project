b5ceb6df873a99dfd7eccde911991afff6eb664d
developersazon <developer.sazon@gmail.com>
2023-03-30 15:38:06 +0600

    Customize the frontend Section Tabs for Breakfast, Lunch and Dinner

--- app/Http/Controllers/frontend/HomeController.php
+++ app/Http/Controllers/frontend/HomeController.php
@@ -13,7 +13,10 @@ class HomeController extends Controller
     public function index(){
         $food_items = Food::all();
         $chef_Items = Chef::all();
-        return view('frontend.home', compact('food_items', 'chef_Items'));
+        $breakfast_menu = Food::where('category', 'breakfast')->get();
+        $lunch_menu = Food::where('category', 'Lunch')->get();
+        $dinner_menu = Food::where('category', 'Dinner')->get();
+        return view('frontend.home', compact('food_items', 'chef_Items', 'breakfast_menu', 'lunch_menu', 'dinner_menu'));
     }
 
 }
---
+++ resources/views/frontend/categoryfood.blade.php
@@ -0,0 +1,140 @@
+<!-- ***** Menu Area Starts ***** -->
+    <section class="section" id="offers">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4 text-center">
+                    <div class="section-heading">
+                        <h6>Klassy Week</h6>
+                        <h2>This Week’s Special Meal Offers</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row" id="tabs">
+                        {{--  breakfast menu tabs  --}}
+                        <div class="col-lg-12">
+                            <div class="heading-tabs">
+                                <div class="row">
+                                    <div class="col-lg-6 offset-lg-3">
+                                        <ul>
+                                          <li><a href='#tabs-1'><img src="{{ url('assets/images/tab-icon-01.png') }}" alt="">Breakfast</a></li>
+                                          <li><a href='#tabs-2'><img src="{{ url('assets/images/tab-icon-02.png') }}" alt="">Lunch</a></a></li>
+                                          <li><a href='#tabs-3'><img src="assets/images/tab-icon-03.png" alt="">Dinner</a></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <section class='tabs-content'>
+                                <article id='tabs-1'>
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="row">
+                                                <div class="left-list d-flex">
+                                                    <div class="col-lg-6">
+                                                        <div class="tab-item">
+                                                            <img src="assets/images/tab-item-01.png" alt="Food Image">
+                                                            <h4>This is the Test Food</h4>
+                                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                                                            <div class="price">
+                                                                <h6>55</h6>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-lg-6">
+                                                        <div class="tab-item">
+                                                            <img src="assets/images/tab-item-01.png" alt="Food Image">
+                                                            <h4>This is the Test Food</h4>
+                                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                                                            <div class="price">
+                                                                <h6>55</h6>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article id='tabs-2'>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="row">
+                                                <div class="left-list">
+                                                    <div class="col-lg-12">
+                                                        <div class="tab-item">
+                                                            <img src="assets/images/tab-item-04.png" alt="">
+                                                            <h4>Eggs Omelette</h4>
+                                                            <p>Lorem ipsum dolor sit amet, consectetur koit adipiscing elit, sed do.</p>
+                                                            <div class="price">
+                                                                <h6>$14</h6>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="row">
+                                                <div class="right-list">
+                                                    <div class="col-lg-12">
+                                                        <div class="tab-item">
+                                                            <img src="assets/images/tab-item-01.png" alt="">
+                                                            <h4>Fresh Chicken Salad</h4>
+                                                            <p>Lorem ipsum dolor sit amet, consectetur koit adipiscing elit, sed do.</p>
+                                                            <div class="price">
+                                                                <h6>$10</h6>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>
+                                <article id='tabs-3'>
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="row">
+                                                <div class="left-list">
+                                                    <div class="col-lg-12">
+                                                        <div class="tab-item">
+                                                            <img src="assets/images/tab-item-02.png" alt="">
+                                                            <h4>Fruit Salad</h4>
+                                                            <p>Lorem ipsum dolor sit amet, consectetur koit adipiscing elit, sed do.</p>
+                                                            <div class="price">
+                                                                <h6>$10</h6>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="row">
+                                                <div class="right-list">
+                                                    <div class="col-lg-12">
+                                                        <div class="tab-item">
+                                                            <img src="assets/images/tab-item-04.png" alt="">
+                                                            <h4>Omelette & Cheese</h4>
+                                                            <p>Lorem ipsum dolor sit amet, consectetur koit adipiscing elit, sed do.</p>
+                                                            <div class="price">
+                                                                <h6>$11</h6>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Chefs Area Ends ***** -->
